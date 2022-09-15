5ca584498211ca81d49d873b3e10ed5d4cbaff5d
EgorMedvedevPomidorka2605 <Pomidorka2605@gmail.com>
2022-09-15 23:02:31 +0700

    Добавьте файлы проекта.

---
+++ LABA  1.2/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace LABA__1._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            GraphPane pane = zedGraphControl1.GraphPane;
+            pane.CurveList.Clear();
+            PointPairList list = new PointPairList();
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
